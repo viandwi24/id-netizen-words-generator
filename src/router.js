7a7f968b748e8efb95df944172a7fe0d8fa39b76
viandwi24 <fiandwi0424@gmail.com>
2020-01-18 20:59:15 +0700

    update

--- src/router.js
+++ src/router.js
@@ -6,6 +6,8 @@ import HomePage from './components/Home.vue'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
+    // base: '/id-netizen-words-generator',
+    // mode: 'hash',
     mode: 'history',
     routes: [
         {
